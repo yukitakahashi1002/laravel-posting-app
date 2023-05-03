5e49278a29b77e807204bf1c20fa341023aa44ff
yukitakahashi1002 <yukitakahashi1002@gmail.com>
2023-05-03 22:55:06 +0900

    0502

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,61 +1,32 @@
-<!DOCTYPE html>
-<html lang="ja">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>新規投稿</title>
+@section('title','新規投稿')
 
-    @vite(['resources/js/app.js'])
-</head>
+@section('content')
+    @if ($errors->any()) 
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach   
+            </ul>
+        </div>
+    @endif
 
-<body style="padding: 60px 0;">
-    <header>
-        <nav class="navbar nabvar-light bg-light fixed-top" style="height: 60px;">
-            <div class="container">
-                <a href="{{ route('posts.index') }}" class="navbar-brand">投稿アプリ</a>
-            </div>
-        </nav>
-    </header>
+    <div class="mb-2">
+        <a href="{{ route('posts.index') }}" class="text-decoration-none">&lt; 戻る</a>
+    </div>
 
-    <main>
-        <article>
-            <div class="container">
-                <h1 class="fs-2 my-3">新規投稿</h1>
-
-                @if ($errors->any()) 
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach   
-                        </ul>
-                    </div>
-                @endif
-
-                <div class="mb-2">
-                    <a href="{{ route('posts.index') }}" class="text-decoration-none">&lt; 戻る</a>
-                </div>
-
-                <form action="{{ route('posts.store') }}" method="post">
-                    @csrf
-                    <div class="form-group mb-3">
-                        <label for="title">タイトル</label>
-                        <input type="text" class="form-control" name="title" value="{{ old('title') }}">
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="content">本文</label>
-                        <textarea class="form-control" name="content">{{ old('content') }}</textarea>
-                    </div>
-                    <button type="submit" class="btn btn-outline-primary">投稿</button>
-                </form>
-            </div>
-        </article>
-    </main>
-
-    <footer class="d-flex justify-content-center align-items-center bg-light fixed-bottom" style="height: 60px;">
-        <p class="text-muted small mb-0">&copy; 投稿アプリ All rights reserved.</p>
-    </footer>
-</body>
-
-</html>
+    <form action="{{ route('posts.store') }}" method="post">
+        @csrf
+        <div class="form-group mb-3">
+            <label for="title">タイトル</label>
+            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+        </div>
+        <div class="form-group mb-3">
+            <label for="content">本文</label>
+            <textarea class="form-control" name="content">{{ old('content') }}</textarea>
+        </div>
+        <button type="submit" class="btn btn-outline-primary">投稿</button>
+    </form>
+@endsection
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,62 +1,33 @@
-<!DOCTYPE html>
-<html lang="ja">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>投稿一覧あああ</title>
+@section('title', '投稿一覧')
 
-    @vite(['resources/js/app.js'])
-</head>
+@section('content')
+    @if (session('flash_message'))
+        <p clas="text-success">{{ session('flash_message') }}</p>
+    @endif
 
-<bodY style="padding: 60px 0;">
-    <header>
-        <nav class="navbar navbar-light bg-light fixed-top" style="height: 60px;">
-            <div class="container">
-                <a href="{{ route('posts.index')}}" class="navbar-brand">投稿アプリ</a>
-            </div>
-        </nav>
-    </header>
+    <div class="mb-2">
+        <a href="{{ route('posts.create') }}" class="text-decoration-none">新規投稿</a>
+    </div>
 
-    <main>
-        <article>
-            <div class="container">
-                <h1 class="fs-2 my-3">投稿一覧</h1>
+    @foreach($posts as $post)
+        <div class="card mb-s">
+            <div class="card-body">
+                <h2 class="card-title fs-5">{{ $post->title }}</h2>
+                <p class="card-text">{{ $post->content }}</p>
 
-                @if (session('flash_message'))
-                    <p clas="text-success">{{ session('flash_message') }}</p>
-                @endif
+                <div class="d-flex">
+                    <a href="{{ route('posts.show', $post) }}" class="btn btn-outline-primary d-block me-1">詳細</a>
+                    <a href="{{ route('posts.edit', $post) }}" class="btn btn-outline-primary d-block me-1">編集</a>
 
-                <div class="mb-2">
-                    <a href="{{ route('posts.create') }}" class="text-decoration-none">新規投稿</a>
+                    <form action="{{ route('posts.destroy', $post) }}" method="post">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="btn btn-outline-danger">削除</button>
+                    </form>
                 </div>
-
-                @foreach($posts as $post)
-                    <div class="card mb-s">
-                        <div class="card-body">
-                            <h2 class="card-title fs-5">{{ $post->title }}</h2>
-                            <p class="card-text">{{ $post->content }}</p>
-
-                            <div class="d-flex">
-                                <a href="{{ route('posts.show', $post) }}" class="btn btn-outline-primary d-block me-1">詳細</a>
-                                <a href="{{ route('posts.edit', $post) }}" class="btn btn-outline-primary d-block me-1">編集</a>
-
-                                <form action="{{ route('posts.destroy', $post) }}" method="post">
-                                    @csrf
-                                    @method('delete')
-                                    <button type="submit" class="btn btn-outline-danger">削除</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
             </div>
-        </article>
-    </main>
-
-    <footer class="d-flex justify-content-center align-items-center bg-light fixed-bottom" style="height: 60px;">
-        <p class="text-muted small mb-0">&copy; 投稿アプリ All rights reserved.</p>
-    </footer>
-</bodY>
-
-</html>
+        </div>
+    @endforeach
+@endsection
